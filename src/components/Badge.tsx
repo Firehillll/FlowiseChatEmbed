e876d908e12870e3b9c8c3eaa9b4da6c6c7d4536
Hilario <hilario@thefabricant.com>
2024-01-22 19:54:03 +0100

    change chatbot text

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Gemaakt door
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://zorgbuddy.webflow.io'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> ZorgBuddy</span>
       </a>
     </span>
   );
